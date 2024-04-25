a841e589c06818b3614504938862b235d8daf547
Sahdab Ansari <sahdabansari972@gmail.com>
2024-04-25 21:11:53 +0530

    area of rectangle

--- function.js
+++ function.js
@@ -1,3 +1,12 @@
 function greet(name){
-    return `hellow ${name}`
-}g
+    //return `hellow ${name}`;
+    console.log("hello ",name)
+}
+greet("shadab")
+// console.log(greet("shadab"))
+
+function areaOfRectangel(a,  b){
+    let area = a*b
+    console.log(`area of rectange is: ${area} `)
+}
+areaOfRectangel(4,5)
